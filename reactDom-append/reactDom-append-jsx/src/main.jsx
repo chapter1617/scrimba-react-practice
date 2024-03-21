4c85a0eea5ef22e0d1efbea4f6be668fe2bfbf24
chapter1617 <goyal.mansi16@gmail.com>
2024-03-21 12:12:56 -0700

    react-practice-append-render

---
+++ reactDom-append/reactDom-append-jsx/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+const page = (
+  <div>
+      <h1>My awesome website in React</h1>
+      <h3>Reasons I love React</h3>
+      <ol>
+          <li>It's composable</li>
+          <li>It's declarative</li>
+          <li>It's a hireable skill</li>
+          <li>It's actively maintained by skilled people</li>
+      </ol>
+  </div>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ page,
+)
